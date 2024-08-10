8605eb151e3b030b061a6a7511a534b05a4ebd28
Raunier24 <peplisoft@gmail.com>
2024-08-11 01:57:34 +0200

    Project updated

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -1,13 +1,39 @@
 package com.fizzbuzz;
 
+import java.util.Scanner;
 
- 
 public class App {
-
     public static void main(String[] args) {
-        System.out.println("Wellcome to my FizzBuzz!");
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.number(2);
+        Scanner scanner = new Scanner(System.in);
         
+        int number = 0;
+                
+        do {
+            System.out.println("Write a number from 1 to 100: ");
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                if (number < 1 || number > 100) {
+                    System.out.println("Please, the number should be in the range of 1 to 100.");
+                }
+            } else {
+                System.out.println("Invalid entry. Please enter a number.");
+                scanner.next();
+            }
+        } while (number < 1 || number > 100);
+
+        System.out.println("Outcome: " + fizzBuzz(number));
+        scanner.close();
+    }
+
+    public static String fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(number);
+        }
     }
 }
--- src/test/java/com/fizzbuzz/AppTest.java
+++ src/test/java/com/fizzbuzz/AppTest.java
@@ -1,17 +1,37 @@
 package com.fizzbuzz;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testFizz() {
+        assertEquals("Fizz", App.fizzBuzz(3));
+        assertEquals("Fizz", App.fizzBuzz(6));
+        assertEquals("Fizz", App.fizzBuzz(9));
+    }
+
+    @Test
+    public void testBuzz() {
+        assertEquals("Buzz", App.fizzBuzz(5));
+        assertEquals("Buzz", App.fizzBuzz(10));
+        assertEquals("Buzz", App.fizzBuzz(20));
+    }
+
+    @Test
+    public void testFizzBuzz() {
+        assertEquals("FizzBuzz", App.fizzBuzz(15));
+        assertEquals("FizzBuzz", App.fizzBuzz(30));
+        assertEquals("FizzBuzz", App.fizzBuzz(45));
+    }
+
+    @Test
+    public void testNumber() {
+        assertEquals("1", App.fizzBuzz(1));
+        assertEquals("2", App.fizzBuzz(2));
+        assertEquals("4", App.fizzBuzz(4));
     }
 }
